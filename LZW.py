9ce9a58da045164ed4622925546647181ddd5092
Claire Champernowne <clairec@Claires-Air.hitronhub.home>
2021-04-14 08:35:20 -0700

    still a leetle broken

--- LZW.py
+++ LZW.py
@@ -2,6 +2,9 @@ import sys
 import numpy as np
 
 
+struct_list = []
+
+
 def main(argv):
 
     if(len(argv) == 0):
@@ -14,7 +17,14 @@ def main(argv):
     
     #remove first line of metadata, join rest w/no newline
     seq = "".join(full_fasta[1:])
-    LZW(seq)
+    dictionary = LZW(seq)
+    dot_bracket = ['-' for i in seq]
+    print(str(dot_bracket))
+
+    all_dot_brackets(0,dot_bracket,dictionary,4)
+
+    global struct_list
+    print(str(struct_list))
 #   for seq in range(0, len(full_fasta), 2):
 #       print(seq)
 #       LZW(full_fasta[seq:seq+2])
@@ -34,7 +44,7 @@ def flip_key(key):
             key_flipped = key_flipped + 'C'
         elif char == 'T':  # remove from final version
             key_flipped = key_flipped + 'A'
-    return key_flipped
+    return key_flipped[::-1]
 
 
 def test_metrics(dictionary, seq):
@@ -55,20 +65,20 @@ def test_metrics(dictionary, seq):
         if len(key) >= 12:
             dict_12plus[key] = dictionary[key]
     
-    per_vals = []
-    for key in dict_13:
-        dict_num = len(dict_13[key][1])
-        seq_num = seq.count(key)
-        if dict_num > seq_num:
-            dict_notable[key] = [dict_num,seq_num]
-        else:
-            per_vals.append((dict_num/seq_num)*100)
+    # per_vals = []
+    # for key in dict_13:
+    #     dict_num = len(dict_13[key][1])
+    #     seq_num = seq.count(key)
+    #     if dict_num > seq_num:
+    #         dict_notable[key] = [dict_num,seq_num]
+    #     else:
+    #         per_vals.append((dict_num/seq_num)*100)
 
-    print(str(len(dict_13.keys())) + ' keys of len 1 - 3 found instances with an avg accuracy of ' + str(sum(per_vals)/len(dict_13.keys())) + '% accuracy')
+    # print(str(len(dict_13.keys())) + ' keys of len 1 - 3 found instances with an avg accuracy of ' + str(sum(per_vals)/len(dict_13.keys())) + '% accuracy')
 
     per_vals = []
     for key in dict_47:
-        dict_num = len(dict_47[key][1])
+        dict_num = len(dict_47[key])
         seq_num = seq.count(key)
         if dict_num > seq_num:
             dict_notable[key] = [dict_num,seq_num]
@@ -78,7 +88,7 @@ def test_metrics(dictionary, seq):
 
     per_vals = []
     for key in dict_811:
-        dict_num = len(dict_811[key][1])
+        dict_num = len(dict_811[key])
         seq_num = seq.count(key)
         if dict_num > seq_num:
             dict_notable[key] = [dict_num,seq_num]
@@ -88,7 +98,7 @@ def test_metrics(dictionary, seq):
 
     per_vals = []
     for key in dict_12plus:
-        dict_num = len(dict_12plus[key][1])
+        dict_num = len(dict_12plus[key])
         seq_num = seq.count(key)
         if dict_num > seq_num:
             dict_notable[key] = [dict_num,seq_num]
@@ -128,23 +138,52 @@ def opposing_keys_check(dictionary, seq):
             print(key + ' exists in sequence ' + str(seq.count(key)) + ' times. ' + flip_key(key) + ' exists in sequence ' + str(seq.count(flip_key(key))) + ' times. ')
 
 
-def stem_match(dictionary, threshold, seq):
-    stem_keys = []
+def expand_dict(dictionary,threshold):
+    for x in range(5):
+        dict_copy = dictionary.copy()
+        for key in dictionary:  # iterate through each key in dict
+            if len(key) >= threshold:  # if length of key is over the threshold, begin to break down
+                if key[1:] in dictionary:  # if subkey removing first ele of key is already in dictionary, append the indeces of key location to list
+                    dict_copy[key[1:]] = list(dict.fromkeys(dictionary[key[1:]] + dictionary[key]))
+                else:  # else, create dictionary entry for subkey which contains curr keys indeces
+                    dict_copy[key[1:]] = list(dict.fromkeys(dictionary[key]))
+
+                to_append = [i - 1 for i in dictionary[key[:-1]]]  # alter indeces for shortened key
+
+                if key[:-1] in dictionary:  # if subkey removing last ele of key is already in dictionary, append the altered indexes of key location to list
+                    dict_copy[key[:-1]] = list(dict.fromkeys(dictionary[key[:-1]] + to_append))
+                else:  # else, create dictionary entry for subkey which contains altered indeces
+                    dict_copy[key[:-1]] = list(dict.fromkeys(to_append))
+        dictionary = dict_copy
+    return dictionary
+
+
+def all_dot_brackets(i,dot_bracket,dictionary,threshold):
+    if '-' not in dot_bracket:
+        global struct_list
+        struct_list.append([dot_bracket])
+        return
+
     for key in dictionary:
-        if len(key) >= threshold:
-            stem_keys.append([key,'FALSE'])
-
-    print(str(len(stem_keys)) + ' keys over length ' + str(threshold))
-
-    for ele in stem_keys:
-        if seq.count(flip_key(ele[0])) > 0 or seq.count(flip_key(ele[0])[::-1]) > 0:
-            ele[1] = 'SHOULD BE TRUE'
-        if flip_key(ele[0]) in dictionary:
-            ele[1] = 'TRUE1'
-        elif flip_key(ele[0])[::-1] in dictionary:
-            ele[1] = 'TRUE2'
-
-    return stem_keys
+        try: 
+            if len(key) >= threshold and dictionary[flip_key(key)]:
+                for index in dictionary[key]:
+                    if index - len(key) -1 == i and dot_bracket[index - len(key) -1:index].count('-') == len(dot_bracket[index - len(key) -1:index]):
+                        for match_index in dictionary[flip_key(key)]:
+                            if dot_bracket[match_index - len(key) -1:match_index].count('-') == len(dot_bracket[match_index - len(key) -1:match_index]):
+                                db_copy = dot_bracket.copy()
+                                if index < match_index:
+                                    for x in range(index - len(key) -1, index): db_copy[x] = '('
+                                    for x in range(match_index - len(key) -1, match_index): db_copy[x] = ')'
+                                else:
+                                    for x in range(index - len(key) -1, index): db_copy[x] = ')'
+                                    for x in range(match_index - len(key) -1, match_index): db_copy[x] = '('
+                                all_dot_brackets(index + 1,db_copy,dictionary,threshold)
+        except:
+            continue
+    db_copy = dot_bracket.copy()
+    db_copy[i] = '.'
+    all_dot_brackets(i + 1,db_copy,dictionary,threshold)
 
 
 def LZW(seq):
@@ -164,7 +203,7 @@ def LZW(seq):
             dictionary[char] = [dict_val, []]
             dict_val = dict_val+1
 
-    for i in range(50):
+    for i in range(20):
         buff = ""
     #  buff = buff + seq[1][0]
         output = []
@@ -172,7 +211,7 @@ def LZW(seq):
             buff = buff + char
             #find the longest string W that matches the current input
             try:
-                dictionary[buff][1].append(index)
+                dictionary[buff].append(index)
                 # check if in dict, if in dict add last index?
 
             except:
@@ -181,7 +220,7 @@ def LZW(seq):
                     output.append(buff[:-1])
                     # output.append(dictionary[buff[:-1]])
                     #add W followed by the next symbol in the input to the dictionary
-                    dictionary[buff] = [dict_val, [index]]  #TODO what do we associate with it (value metric + location of last match?
+                    dictionary[buff] = [index]  #TODO what do we associate with it (value metric + location of last match?
                     dict_val = dict_val + 1
                     #remove W from input
                     buff = buff[len(buff)-1:]
@@ -203,16 +242,21 @@ def LZW(seq):
         # print('Pass ' + str(k))
         # test_metrics(dictionary, seq)
 
-    for key in dictionary:  # remove repeat instances of indexes
-        temp_list = []
-        for i in dictionary[key][1]:
-            if i not in temp_list:
-                temp_list.append(i)
-        dictionary[key][1] = temp_list
+    # for key in dictionary:  # remove repeat instances of indexes
+    #     temp_list = []
+    #     for i in dictionary[key]:
+    #         if i not in temp_list:
+    #             temp_list.append(i)
+    #     dictionary[key][1] = temp_list
 
-    print('DICTIONARY: ' + str(dictionary))
-    print('POTENTIAL STEMS: ' + str(stem_match(dictionary,8,seq)))
-    # test_metrics(dictionary, seq)
+    # print('DICTIONARY: ' + str(dictionary))
+    # print('POTENTIAL STEMS: ' + str(stem_match(dictionary,8,seq)))
+    print('BEFOREEE')
+    test_metrics(dictionary, seq)
+    dictionary = expand_dict(dictionary,5)
+    print('AFTERRR')
+    test_metrics(dictionary, seq)
+    return dictionary
     # opposing_keys_check(dictionary, seq)
 
 
